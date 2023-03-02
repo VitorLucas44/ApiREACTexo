34d2ebf7805c8be5cfe4baeae6b839e904893286
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-03-02 15:26:27 +0100

    exo1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
